115ff499be3b86cb1b4a7a413a9c57d076785acc
Federico-Bargosi <bargossifede98@gmail.com>
2025-02-03 15:53:41 +0100

    navbar e footer aggiornati

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
+import {Link} from "react-router-dom"
+
 export function Footer() {
   // Footer
   return (
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3  border-top container-fluid navbar-dark bg-dark mt-5 ">
       <p className="col-md-4 mb-0 text-white">© 2024 Company, Inc</p>
 
-      <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-white">
+      <Link to="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         BoolRoad
-      </a>
+      </Link>
 
-      <ul className="nav col-md-4 justify-content-end text-white gap-3 ">
+      <ul className="nav col-md-4 text-white gap-3 ">
         <li className="nav-item text-decoration-none">Home</li>
         <li className="nav-item text-decoration-none ">Features</li>
         <li className="nav-item text-decoration-none">Pricing </li>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,9 @@ export function Header() {
   return (
     <header className="mb-5">
       <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark ">
-        <div className="container-fluid">
+        <div className="container-fluid flex-nowrap">
           <a className="navbar-brand" href="#">BoolRoad</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item active">
               {location.pathname !== "/" && (
