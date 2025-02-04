b65a9c22aeba35db626edd38c67ffaba19ce8e47
Federico-Bargosi <bargossifede98@gmail.com>
2025-02-04 11:11:25 +0100

    aggiornata nav-bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export function Header() {
     <header className="mb-5">
       <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark ">
         <div className="container-fluid flex-nowrap">
-          <a className="navbar-brand" href="#">BoolRoad</a>
+          <Link className="navbar-brand" to="/">BoolRoad</Link>
           <div className="navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item active">
